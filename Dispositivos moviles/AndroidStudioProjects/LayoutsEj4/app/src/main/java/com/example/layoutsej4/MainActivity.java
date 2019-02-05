3ad587930915ab2e208481f5a7d49079b71b045b
malyga <40186295+malyga@users.noreply.github.com>
2019-02-05 20:54:44 +0100

    ASFASF

---
+++ Dispositivos moviles/AndroidStudioProjects/LayoutsEj4/app/src/main/java/com/example/layoutsej4/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.layoutsej4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        imageView = findViewById(R.id.imageView);
+    }
+
+    public void click(View view) {
+        button.setVisibility(View.INVISIBLE);
+        imageView.setVisibility(View.VISIBLE);
+    }
+}
