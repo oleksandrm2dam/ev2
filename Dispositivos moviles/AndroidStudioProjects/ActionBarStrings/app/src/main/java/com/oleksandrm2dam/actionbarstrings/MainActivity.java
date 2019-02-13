2c569630f5cf6fe217ed8f3966d0b259ed6aaf37
malyga <40186295+malyga@users.noreply.github.com>
2019-02-13 08:25:50 +0100

    actionbarstring

--- Dispositivos moviles/AndroidStudioProjects/ActionBarStrings/app/src/main/java/com/oleksandrm2dam/actionbarstrings/MainActivity.java
+++ Dispositivos moviles/AndroidStudioProjects/ActionBarStrings/app/src/main/java/com/oleksandrm2dam/actionbarstrings/MainActivity.java
@@ -1,17 +1,26 @@
 package com.oleksandrm2dam.actionbarstrings;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonHideShowAB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonHideShowAB = findViewById(R.id.button);
     }
 
     @Override
@@ -23,11 +32,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
-            case R.id.option1:
-                Toast.makeText(this, R.string.menu_option1, Toast.LENGTH_LONG).show();
+            case R.id.optionSpanish:
+                changeLang("es");
                 return true;
-            case R.id.option2:
-                Toast.makeText(this, R.string.menu_option2, Toast.LENGTH_LONG).show();
+            case R.id.optionEnglish:
+                changeLang("en");
                 return true;
             case R.id.phone:
                 Toast.makeText(this, R.string.phone, Toast.LENGTH_LONG).show();
@@ -39,4 +48,25 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    private void changeLang(String code) {
+        Locale locale = new Locale(code);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getResources().updateConfiguration(config, null);
+        Intent refresh = new Intent(MainActivity.this, MainActivity.class);
+        startActivity(refresh);
+        finish();
+    }
+
+    public void showHideAB(View view) {
+        if(getSupportActionBar().isShowing()) {
+            getSupportActionBar().hide();
+            buttonHideShowAB.setText(R.string.show_action_bar);
+        } else {
+            getSupportActionBar().show();
+            buttonHideShowAB.setText(R.string.hide_action_bar);
+        }
+    }
+
 }
